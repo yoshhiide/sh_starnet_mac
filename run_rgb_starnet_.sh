de6c5626ac8b1cf5df810d5314f3d801f0dc8d51
yoshhiide <yoshihide.nabeyama@gmail.com>
2021-08-18 22:29:05 +0900

    number -> stride

--- run_rgb_starnet_.sh
+++ run_rgb_starnet_.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
-# run_rgb_starnet++ infile outfile StarSize(16,32,64...)
+# run_rgb_starnet++ infile outfile Stride(16,32,64,128...)
 
 cd `dirname $0`
 
 if [ $# -ne 2 ]; then
 	if [ $# -ne 3 ]; then
-		echo 'require args is 2 or 3 (args: infile outfile [16,32,64...])'
+		echo 'require args is 2 or 3 (args: infile outfile [Stride:16,32,64,128...])'
 		exit 1;
 	fi
 fi
 
 input=$1
 output=$2
-number=64
+stride=64
 if [ $# -eq 3 ]; then
-	number=$3
+	stride=$3
 fi
 
 
-echo "starnet++ $input $output $number"
-rgb_starnet++ $input $output $number
+echo "starnet++ $input $output $stride"
+rgb_starnet++ $input $output $stride
 
 exit 0
